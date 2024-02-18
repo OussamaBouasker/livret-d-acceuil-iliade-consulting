5369a8b422d47980af176ef22e2e3c76ec09c37e
Oussama <oussamabouasker6@gmail.com>
2024-02-18 12:15:28 +0100

    app-module-change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NotificationComponent } from './notification/notification.component';
+
 import { loginComponent } from './login/login.component';
 
 @NgModule({
@@ -21,7 +21,6 @@ import { loginComponent } from './login/login.component';
     HomeComponent,
     loginComponent,
     FooterComponent,
-    NotificationComponent
   ],
   imports: [
     BrowserModule,
