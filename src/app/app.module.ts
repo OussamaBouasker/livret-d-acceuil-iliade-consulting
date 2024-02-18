ff7ce567ffeb27890249d9ca811a714f41bcf04e
Oussama <oussamabouasker6@gmail.com>
2024-02-18 13:05:55 +0100

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CoursComponent } from './cours/cours.component';
 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent,
     NavbarComponent,
     HomeComponent,
